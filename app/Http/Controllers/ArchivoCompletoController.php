49fe428353c097f5c3f19e907822744a99eec94d
AngelChavarria08 <144398620+AngelChavarria08@users.noreply.github.com>
2024-08-21 09:10:25 -0600

    id usuario conteos

--- app/Http/Controllers/ArchivoCompletoController.php
+++ app/Http/Controllers/ArchivoCompletoController.php
@@ -5,13 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class ArchivoCompletoController extends Controller
 {
     public function getCargasByUsuario($id_usuario)
     {
         $result = DB::table('tab_detalle_cargas')
             ->where('id_usuario', $id_usuario)
-            ->select('id as id_carga')
+            ->select(
+                'id as id_carga',  // Alias para el ID de la carga
+                'cve_carga',       // Nombre correcto de la columna
+                'fecha_asignacion',
+                'conteo',
+                'nombre_archivo',
+                'Reg_Archivo',
+                'Reg_a_Contar',
+                'reg_vobo',
+                'reg_excluidos',
+                'reg_incorpora',
+                'habilitado'
+            )
             ->get();
 
         return response()->json($result);
