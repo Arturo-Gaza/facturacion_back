1e6bfe765235699ea73bb5ba30330a97cbe095db
GabrielHopewellSystem <122858542+GabrielHopewellSystem@users.noreply.github.com>
2024-09-02 17:47:17 -0600

    Revert "correccion"
    This reverts commit 0d93788952f241cff5d3134fe06bda9762035e65.

--- app/Http/Controllers/noInsertarFaltantesController.php
+++ app/Http/Controllers/noInsertarFaltantesController.php
@@ -151,6 +151,11 @@ class noInsertarFaltantesController extends Controller
         $numDatosNoEncontrados2 = count($datoNoEncontrado2);
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        
+       
+       
+
+
         $ultimaCarga = DB::table('tab_detalle_cargas')
             ->select('cve_carga')
             ->orderBy('id', 'desc')
@@ -182,8 +187,8 @@ class noInsertarFaltantesController extends Controller
         $detalleArchivo->observaciones = $request->input('observaciones');
         $detalleArchivo->habilitado = $request->input('habilitado', true);
 
-        //$detalleArchivo->save();
-       
+        $detalleArchivo->save();
+        $this->cargarArchivoCompleto($request, $detalleArchivo);
         $this->insertCatProductos();
         $this->contarRegistros($ultimoId);
 
@@ -206,7 +211,6 @@ class noInsertarFaltantesController extends Controller
         $detalleArchivo2->habilitado = $request->input('habilitado', true);
 
         $detalleArchivo2->save();
-        $this->cargarArchivoCompleto($request, $detalleArchivo2);
         return response()->json(['success' => true, 'message' => 'Los datos no se insertaron en los catalogos', $totalRegistros, 'data' => $detalleArchivo]);
     }
 
