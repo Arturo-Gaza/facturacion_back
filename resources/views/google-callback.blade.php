d7cc13cd312fbc3875572e9bf5b555968c933fa1
OscarAlfredoBustamante <92607920+OscarAlfredoBustamante@users.noreply.github.com>
2025-10-07 13:17:53 -0600

    Update google-callback.blade.php

--- resources/views/google-callback.blade.php
+++ resources/views/google-callback.blade.php
@@ -1,29 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>Google Auth Callback</title>
+    <title>Google Auth Callback</title>
 </head>
 <body>
-  <script>
+<script>
     // Los datos se inyectan desde Laravel
     const data = {
-      type: "google-auth-success", // 👈 importante para identificar el mensaje
-      status: true,
-      user: @json($user),
-      token: "{{ $token }}",
-      tokenGoogle: "{{ $tokenGoogle }}"
+        status: true,
+        user: @json($user),
+        token: "{{ $token }}",
+        tokenGoogle: "{{ $tokenGoogle }}"
     };
+    const targetOrigin = {!! json_encode(request()->query('origin', '*')) !!};
 
-    // El frontend pasó ?origin=http://localhost:5173 o similar
-    const targetOrigin = {!! json_encode(request()->query('origin', 'http://localhost:5173')) !!};
-
-    if (window.opener) {
-      window.opener.postMessage(data, targetOrigin);
-      window.close();
-    } else {
-      document.write("No se pudo comunicar con la aplicación principal.");
-    }
-  </script>
+    // Enviamos los datos a la ventana que abrió el popup
+window.opener.postMessage(data, targetOrigin);
+window.close();
+</script>
 </body>
 </html>
